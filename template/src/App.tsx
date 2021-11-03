7241d2da6d14165c07279d1a76f76dd6cbdf747d
Willian Arsenio de Sousa <willian.arsenio.de.sousa@zalando.de>
2021-11-03 01:01:17 +0100

    feat: include css libs (tailwind, tw, styled-comeponents)

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import tw, from 'twin.macro';
 import './App.css';
+
+const Paragraph = tw.p`text-2xl font-medium text-white`;
+const Link = tw.a`text-base text-blue-200 hover:text-blue-300 font-medium mx-4 cursor-pointer transition-all ease-in duration-300`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Paragraph>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </Paragraph>
+        <Link
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Link>
       </header>
     </div>
   );
--- template/src/AppRouter.tsx
+++ template/src/AppRouter.tsx
@@ -1,18 +1,22 @@
 // src/AppRouter.tsx
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { RecoilRoot } from 'recoil'
+import { GlobalStyles } from 'twin.macro';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 
 const AppRouter = () => (
-  <Router>
-      <RecoilRoot>
-        <Switch>
-          <Route exact path="/" component={App} />
-        </Switch>
-      </RecoilRoot>
-  </Router>
+  <React.Fragment>
+    <GlobalStyles />
+    <Router>
+        <RecoilRoot>
+          <Switch>
+            <Route exact path="/" component={App} />
+          </Switch>
+        </RecoilRoot>
+    </Router>
+  </React.Fragment>
 );
 
 
