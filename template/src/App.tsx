85fc64d5b8cb9eb6a3ae298fc3b2c3951e84cb23
Willian Arsenio de Sousa <willian.arsenio.de.sousa@zalando.de>
2021-11-03 10:32:35 +0100

    feat: include eslintrc

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import tw, from 'twin.macro';
-import './App.css';
+import tw from 'twin.macro';
 
 const Paragraph = tw.p`text-2xl font-medium text-white`;
 const Link = tw.a`text-base text-blue-200 hover:text-blue-300 font-medium mx-4 cursor-pointer transition-all ease-in duration-300`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <Paragraph>
           Edit <code>src/App.tsx</code> and save to reload.
         </Paragraph>
         <Link
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
